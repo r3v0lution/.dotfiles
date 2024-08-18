b029e096db4885992a5a9217f10791cec426c93d
r3v0lution <seifert.jeremy@gmail.com>
2024-08-18 14:05:41 -0500

    added eza to install list

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/zsh
 #
 # install script for first time start up
+sudo apt update
+sudo apt install -y gpg
+
+# install eza
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
 
 
-sudo apt install \
+sudo apt -y eza \
+	install \
 	zsh \
 	git \
 	neovim \
@@ -17,6 +27,8 @@ command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
+
+
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
