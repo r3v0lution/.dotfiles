d65dfe6bdd92e8dcd94a006bca4f968110cf0e98
r3v0lution <seifert.jeremy@gmail.com>
2024-08-18 13:38:09 -0500

    added install ohmyzsh and p10k

--- install.sh
+++ install.sh
@@ -12,17 +12,19 @@ sudo apt install \
 	ripgrep \
 	bat \
 
-# stow .dotfiles
-
-stow nvim
-stow zsh
-stow p10k
-
 # add zsh to login shells
 command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# stow .dotfiles
+stow nvim
+stow zsh
+stow p10k
 
 # install neovim plugins
 nvim --headless +PlugInstall +qall
